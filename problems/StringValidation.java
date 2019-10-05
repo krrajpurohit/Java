4fa59ea0fd7baf3fbaed5beea7450219f8acb2bb
krrajpurohit <kr890794@gmail.com>
2019-10-05 23:04:37 +0530

    StringValidation initial character bug fixed

--- problems/StringValidation.java
+++ problems/StringValidation.java
@@ -7,7 +7,7 @@ public class StringValidation {
 
 	public boolean isStringValid(String input) {
 
-		if (input.length() % 2 != 0) {
+		if (input.length() % 2 != 0 || input.charAt(0) == '>' || input.charAt(0) == ']') {
 			return false;
 		} else {
 			for (int i = 0; i < input.length(); i++) {
@@ -28,7 +28,7 @@ public class StringValidation {
 
 	public static void main(String[] args) {
 
-		String input = "<[<[[<[]>[<>]]]>]>";
+		String input = "><[]<>";
 		StringValidation test = new StringValidation();
 		System.out.println(test.isStringValid(input));
 
